a019b2063c3c6efd2ce6cef615dd6af1d0c1eaf6
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-10-03 20:37:08 -0300

    adc segundo requisito do projeto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,82 @@
 import React from 'react';
 import './styles/login.css';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nameInput: '',
+      isLoading: false,
+    };
+  }
+
+  onChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [target.name]: value });
+  };
+
+  isDisabled = () => {
+    const { nameInput } = this.state;
+    const minLength = 3;
+
+    return (nameInput.length < minLength);
+  };
+
+  handleClick = async () => {
+    const { nameInput } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name: nameInput });
+    history.push('/search');
+  };
+
   render() {
+    const { nameInput, isLoading } = this.state;
+
+    const form = (
+      <>
+        <h1>Trybe Tunes</h1>
+        <form>
+          <input
+            type="text"
+            name="nameInput"
+            placeholder="nome"
+            value={ nameInput }
+            onChange={ this.onChange }
+            data-testid="login-name-input"
+          />
+          <button
+            type="button"
+            className="button-28"
+            data-testid="login-submit-button"
+            disabled={ this.isDisabled() }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </>
+    );
+
+    const loading = (
+      <p>
+        Carregando...
+      </p>
+    );
+
     return (
       <div data-testid="page-login" className="page-login">
-        <h1>teste Login</h1>
+        {isLoading ? loading : form}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default Login;
