bd79d1718c4a69f0266b177722a8b28975b2845f
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-10-03 23:25:52 -0300

    adc quinto requisito do projeto

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import './styles/login.css';
+import './styles/search.css';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [target.name]: value });
+  };
+
+  isDisabled = () => {
+    const { searchInput } = this.state;
+    const minLength = 2;
+
+    return (searchInput.length < minLength);
+  };
+
   render() {
+    const { searchInput } = this.state;
+
     return (
       <>
         <Header />
         <div data-testid="page-search" className="page-search">
-          <h1>teste Search</h1>
+          <h1>Search</h1>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="searchInput"
+            value={ searchInput }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            className="button-28"
+            data-testid="search-artist-button"
+            disabled={ this.isDisabled() }
+          >
+            Pesquisar
+          </button>
         </div>
       </>
     );
