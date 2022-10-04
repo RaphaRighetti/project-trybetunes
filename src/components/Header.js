93576d7a243bf21da56b50e4996f7ba87f03d751
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-10-03 21:58:37 -0300

    adc quarto requisito do projeto

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import './components.css';
 
@@ -20,12 +21,46 @@ class Header extends Component {
     const { user } = this.state;
 
     return (
-      <header data-testid="header-component" className="header">
-        <h2>Trybe Tunes</h2>
-        <span data-testid="header-user-name">
-          {user.name ? user.name : 'Carregando...'}
-        </span>
-      </header>
+      <>
+        <header data-testid="header-component" className="header">
+          <h2>Trybe Tunes</h2>
+          <span data-testid="header-user-name">
+            {user.name ? user.name : 'Carregando...'}
+          </span>
+        </header>
+        <nav className="navigation">
+          <span>
+            <NavLink
+              to="/search"
+              data-testid="link-to-search"
+              activeClassName="activeLink"
+              className="link"
+            >
+              Search
+            </NavLink>
+          </span>
+          <span>
+            <NavLink
+              to="/favorites"
+              data-testid="link-to-favorites"
+              activeClassName="activeLink"
+              className="link"
+            >
+              Favorites
+            </NavLink>
+          </span>
+          <span>
+            <NavLink
+              to="/profile"
+              data-testid="link-to-profile"
+              activeClassName="activeLink"
+              className="link"
+            >
+              Profile
+            </NavLink>
+          </span>
+        </nav>
+      </>
     );
   }
 }
