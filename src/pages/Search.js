20c4c997b7df21f4865f4f9859b4e0c10a0cd65b
RaphaelRighetti <raphaelrighetti@hotmail.com>
2022-10-04 00:49:13 -0300

    adc sexto requisito do projeto

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles/search.css';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -8,6 +10,10 @@ class Search extends React.Component {
 
     this.state = {
       searchInput: '',
+      isLoading: false,
+      showSearch: false,
+      albuns: [],
+      searchName: '',
     };
   }
 
@@ -23,8 +29,45 @@ class Search extends React.Component {
     return (searchInput.length < minLength);
   };
 
-  render() {
+  handleClick = async () => {
+    this.setState({ isLoading: true, showSearch: false });
     const { searchInput } = this.state;
+    const albuns = await searchAlbumsAPI(searchInput);
+    this.setState((prev) => ({
+      albuns,
+      isLoading: false,
+      showSearch: true,
+      searchInput: '',
+      searchName: prev.searchInput,
+    }));
+  };
+
+  render() {
+    const { searchInput, isLoading, showSearch, albuns, searchName } = this.state;
+    const notFound = 'Nenhum álbum foi encontrado';
+    const found = `Resultado de álbuns de: ${searchName}`;
+    const albunsCard = (
+      <>
+        <p>{albuns.length === 0 ? notFound : found}</p>
+        {albuns.map((element) => (
+          <Link
+            to={ `/album/${element.collectionId}` }
+            className="albumLink"
+            key={ element.collectionId }
+            data-testid={ `link-to-album-${element.collectionId}` }
+          >
+            <div className="albumCard">
+              <img
+                src={ element.artworkUrl100 }
+                alt={ element.collectionName }
+              />
+              <p className="nomeAlbum">{element.collectionName}</p>
+              <p className="artist">{element.artistName}</p>
+            </div>
+          </Link>
+        ))}
+      </>
+    );
 
     return (
       <>
@@ -37,15 +80,21 @@ class Search extends React.Component {
             name="searchInput"
             value={ searchInput }
             onChange={ this.handleChange }
+            placeholder="Nome do artista"
           />
           <button
             type="button"
             className="button-28"
             data-testid="search-artist-button"
             disabled={ this.isDisabled() }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
+        </div>
+        <div className="albunsDiv">
+          {isLoading && (<h2>Carregando...</h2>)}
+          {showSearch && albunsCard}
         </div>
       </>
     );
